f1455ffb33af638cb1e2fd797331f805ccae6baf
BeauJoh <beau@inbeta.org>
2011-07-13 11:41:29 +1000

    Initial Commit

---
+++ RGBAUtilities/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RGBAUtilities
+//
+//  Created by Beau Johnston on 13/07/11.
+//  Copyright 2011 University Of New England. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
